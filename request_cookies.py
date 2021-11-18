1028eb188469faeb441598bb1b99e3a1cf5fb4f9
Vladimir Sarychev <svmyhome@mail.ru>
2021-11-18 14:10:12 +0300

    Master: 1. Добален файл с авторизацией через cookies

---
+++ request_cookies.py
@@ -0,0 +1,17 @@
+import requests
+
+payload = {"login": "secret_login", "password": "secret_pass"}
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)  # для GET используем params, а для POST используем data
+print(response1.text)
+print(response1.status_code)
+print(response1.cookies)
+print(dict(response1.cookies))
+print(response1.headers)
+print("-" * 10)
+cookie_value = response1.cookies.get("auth_cookie")
+cookie = {}
+if cookie is not None:
+    cookie.update({"auth_cookie": cookie_value})
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookie)
+print(response2.text)
+print(response2.status_code)
